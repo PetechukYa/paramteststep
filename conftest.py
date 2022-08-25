857b8c64d9495a5ed77fe0d6134699a8a783dbde
Yaroslav Petechuk <yapetechuk@gmail.com>
2022-08-25 15:55:23 +0300

    initial commit + exercise for review

---
+++ conftest.py
@@ -0,0 +1,30 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+@pytest.fixture(scope="function")
+def browser(language):
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    print("\nStart browser for test..")
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print("\nQuit browser..")
+    browser.quit()
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--language",
+        action='store',
+        default="en-gb",
+        help="Choose lang en-gb or uk "
+    )
+
+
+@pytest.fixture(scope="function")
+def language(request):
+    language = request.config.getoption("--language")
+    return language
+
---
+++ test_items.py
@@ -0,0 +1,16 @@
+from  time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def test_is_button_buy_disp(browser):
+    browser.get(f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+    sleep(10)
+    browser.implicitly_wait(5)
+    button = WebDriverWait(browser, 5).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "button[class*='btn-add-to-basket']")))
+    assert button.text is not None, "Текст у кнопки відсутній"
+
+
